cb7aee643c02beede609e17bdecead5feaec5cbf
jpenagos32 <jpenagosdev@gmail.com>
2024-06-25 22:55:39 -0500

    proyecto terminado

---
+++ index.js
@@ -0,0 +1,36 @@
+let textArea = document.getElementById('texto')
+let paragraph = document.getElementById('texto-encriptado')
+
+const encriptar = () => {
+  const text = textArea.value
+
+  const replacedText = text
+    .replace(/e/g, 'enter')
+    .replace(/i/g, 'imes')
+    .replace(/a/g, 'ai')
+    .replace(/o/g, 'ober')
+    .replace(/u/g, 'ufat')
+
+  paragraph.innerHTML = replacedText
+}
+
+const desencriptar = () => {
+  const text = textArea.value
+
+  const decriptedText = text
+    .replace(/enter/g, 'e')
+    .replace(/imes/g, 'i')
+    .replace(/ai/g, 'a')
+    .replace(/ober/g, 'o')
+    .replace(/ufat/g, 'u')
+
+  paragraph.innerHTML = decriptedText
+}
+
+const footerParagraph = document.getElementById('footer-copy')
+const actualYear = new Date().getFullYear()
+let footerText = 'Copy '
+footerText += '\u00A9 '
+footerText += actualYear
+footerText += ' Julian Andres Penagos'
+footerParagraph.innerHTML = footerText
